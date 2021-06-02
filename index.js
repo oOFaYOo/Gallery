cb4847994a2c859b15b0d999ce30f2376a59df82
oOFaYOo <nika.tseleva@mail.ru>
2021-06-03 01:18:56 +0500

    made switching pictures in view mode

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 let close = document.getElementById("close");
 let pictures = document.getElementsByClassName("pic_ico");
 let image = document.getElementById("origin_pic");
+let leftArrow = document.getElementById("left");
+let rightArrow = document.getElementById("right");
+
+let originImageNow = {
+    number: null,
+    name: null,
+};
 
 close.onclick = () => {
     document.getElementById("pic_big").hidden = true;
+    image.src = "";
+    originImageNow.number = null;
+    originImageNow.name = null;
 }
 console.dir();
 
-// for(let pic of pictures){
-//     pic.onclick = () => {
-//         document.getElementById("pic_big").hidden = false;
-//     }
-// }
 
-for(let pic of pictures){
-    pic.onclick = () => {
-        image.src = pic.firstChild.src;
+for(let i = 0; i < pictures.length; i++){
+    pictures[i].onclick = () => {
+        originImageNow.number = i;
+        originImageNow.name = pictures[originImageNow.number].firstChild.src;
+        image.src = originImageNow.name;
         document.getElementById("pic_big").hidden = false;
     }
 }
+
+leftArrow.onclick = () => {
+    if(originImageNow.number > 0){
+        arrowLeft();
+    }
+};
+
+rightArrow.onclick = () => {
+    if(originImageNow.number < pictures.length-1){
+        arrowRight();
+    }
+}
+
+function arrowLeft() {
+    originImageNow.number = originImageNow.number-1;
+    originImageNow.name =  pictures[originImageNow.number].firstChild.src;
+    image.src = originImageNow.name;
+}
+
+function arrowRight() {
+    originImageNow.number = originImageNow.number+1;
+    originImageNow.name =  pictures[originImageNow.number].firstChild.src;
+    image.src = originImageNow.name;
+}
+
